28073357b43c7b45f6d1b306d8190e17682c083b
major-winter <tuanhoangchu@outlook.com>
2021-02-08 19:44:14 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,10 +18,12 @@ app.use(cartRoutes)
 
 
 // static folder
-app.use(express.static(path.join(__dirname, './public')))
+const publicDir = path.join(__dirname, '../public')
+const spaDir = path.join(__dirname, '../public', 'index.html')
+app.use(express.static(publicDir))
 
 // Handle SPA
-app.get('/*', (req, res) => res.sendFile(path.join((__dirname, './public', 'index.html'))))
+app.get('/*', (req, res) => res.sendFile(spaDir))
 
 
 const port = process.env.PORT || 5005
