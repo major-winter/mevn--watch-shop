7e9526ac6da01e6d70ef851e66bf48988cde31a0
major-winter <tuanhoangchu@outlook.com>
2021-02-08 20:39:44 +0700

    update

--- backend/src/mongodb.js
+++ backend/src/mongodb.js
@@ -2,7 +2,7 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectionURL = process.env.MONGODB_URL
+const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'MEVN-watch-shop'
 
 const products = require('../data/sampleData')
